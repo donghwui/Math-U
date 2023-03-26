a7cd484c89d8834a4eec5f3a1d228129905b3689
adam <109483705+adam-han@users.noreply.github.com>
2023-03-26 00:26:06 -0400

    a fully functioning chatbot

--- chatbot.py
+++ chatbot.py
@@ -4,21 +4,25 @@ import prompt_formatter as pf
 # API Key (pls don't show anyone, money is attached to this)
 openai.api_key = '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'
 
-# boolean for determining when to stop
-end = False
+# randomness level of responses (between 0 to 2, with 2 being more random)
+randomness_level = 0.4
+
+
+# asks user to choose a major
+major = input("Enter Major You're Interested In: ")
 
 # handles the opening message
 def opening():
-    # asks user to choose a major
-    major = input("Enter Major You're Interested In: ")
-
     # gets initial prompt
     initial_prompt = pf.get_prompt(major)
 
     # opening message
     completion = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
-    messages=[{"role": "user", "content": initial_prompt}]
+    messages=[{"role": "user", "content": initial_prompt}],
+    presence_penalty=0,
+    frequency_penalty=0,
+    temperature=randomness_level           # handles the "randomness" of responses
     )
 
     response = completion.choices[0].message.content
@@ -26,17 +30,12 @@ def opening():
 
 
 # handles asking user for their question and outputting response
-def ask_and_response():
-    # asks user to enter their question
-    question = input("Please Enter Your Question: ")
-
-    # checks to end program
-    if question == "end":
-        end = True
+def ask_and_response(question):
+    prompt = pf.formatq(major, question)
 
     completion = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
-    messages=[{"role": "user", "content": question}]
+    messages=[{"role": "user", "content": prompt}]
     )
 
     response = completion.choices[0].message.content
@@ -45,6 +44,16 @@ def ask_and_response():
 
 
 opening()
-while not end:
-    ask_and_response()
+while True:
+    print("\n-------------------\n")
+
+    # asks user to enter their question
+    question = input("Please Enter Your Question: ")
+
+    # exits program if user input is "exit"
+    if question == "exit":
+        break
+
+    ask_and_response(question)
+
 
--- prompt_formatter.py
+++ prompt_formatter.py
@@ -8,7 +8,7 @@ def get_prompt(major):
         first_line = data.readline()
 
         # first part of prompt ----
-        prompt += (f"chatgpt, you are now an academic advisor with the purpose of answering any questions given by a student\nYou have this knowledge: \nThe course requirements for a {major} major are:\n")
+        prompt += (f"you are now an academic advisor with the purpose of answering any questions given by a student\nYou have this knowledge: \nThe course requirements for a {major} major are:\n")
         prompt += ("--------------------------------------------")
 
 
@@ -29,3 +29,32 @@ def get_prompt(major):
         prompt += ("Your answers will be based only on the context provided above with no other prior information. All you answers should be formatted as if you were talking to a student directly. Do not mention anything about having the context given above. Please start by asking the student if they have any questions")
     return prompt
 
+
+def formatq(major, question):
+    prompt = ""
+
+    with open(f"./majors_data/{major}.txt", 'r') as data:
+        first_line = data.readline()
+
+        # first part of prompt ----
+        prompt += (f"you are now an academic advisor with the purpose of answering any questions given by a student\nYou have this knowledge: \nThe course requirements for a {major} major are:\n")
+        prompt += ("--------------------------------------------")
+
+
+        # context for prompt ----
+
+        # checks if we need to include pre-requirements from table 2
+        if "! include table 2" in first_line:
+            with open("./majors_data/table2.txt", 'r') as table2:
+                table2_contents = table2.read()
+                prompt += (table2_contents)
+
+        file_contents = data.read()
+        prompt += (file_contents)
+
+
+        # end of prompt ----
+        prompt += ("--------------------------------------------\n")
+        prompt += ("Your answers will be based only on the context provided above with no other prior information. All you answers should be formatted as if you were talking to a student directly. Do not mention anything about having the context or list given above.")
+        prompt += (f"answer the question, {question}")
+    return prompt
