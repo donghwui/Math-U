9ab1c55b10feee0077b0a1574c23be287211be06
adam <109483705+adam-han@users.noreply.github.com>
2023-03-25 20:30:04 -0400

    working chatbot! issue: does not have memory of previous responses

--- chatbot.py
+++ chatbot.py
@@ -4,17 +4,47 @@ import prompt_formatter as pf
 # API Key (pls don't show anyone, money is attached to this)
 openai.api_key = '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'
 
-# asks user to choose a major
-major = input("Enter Major You're Interested In: ")
+# boolean for determining when to stop
+end = False
 
-# gets initial prompt
-initial_prompt = pf.get_prompt(major)
+# handles the opening message
+def opening():
+    # asks user to choose a major
+    major = input("Enter Major You're Interested In: ")
 
-# opening message
-completion = openai.ChatCompletion.create(
-  model="gpt-3.5-turbo",
-  messages=[{"role": "user", "content": initial_prompt}]
-)
+    # gets initial prompt
+    initial_prompt = pf.get_prompt(major)
 
-response = completion.choices[0].message.content
-print(response)
+    # opening message
+    completion = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[{"role": "user", "content": initial_prompt}]
+    )
+
+    response = completion.choices[0].message.content
+    print(response)
+
+
+# handles asking user for their question and outputting response
+def ask_and_response():
+    # asks user to enter their question
+    question = input("Please Enter Your Question: ")
+
+    # checks to end program
+    if question == "end":
+        end = True
+
+    completion = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[{"role": "user", "content": question}]
+    )
+
+    response = completion.choices[0].message.content
+    print(response)
+
+
+
+opening()
+while not end:
+    ask_and_response()
+
