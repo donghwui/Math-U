ad2b0c48e355f19aeeeff0059127a11ce1256dd0
adam <109483705+adam-han@users.noreply.github.com>
2023-03-25 20:13:37 -0400

    first working integration with chatgpt api

--- chatbot.py
+++ chatbot.py
@@ -1,12 +1,20 @@
 import openai
+import prompt_formatter as pf
 
+# API Key (pls don't show anyone, money is attached to this)
 openai.api_key = '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'
 
+# asks user to choose a major
+major = input("Enter Major You're Interested In: ")
 
+# gets initial prompt
+initial_prompt = pf.get_prompt(major)
+
+# opening message
 completion = openai.ChatCompletion.create(
   model="gpt-3.5-turbo",
-  messages=[{"role": "user", "content": "What is the circumference in km of the planet Earth?"}]
+  messages=[{"role": "user", "content": initial_prompt}]
 )
 
-reply_content = completion.choices[0].message.content
-print(reply_content)
+response = completion.choices[0].message.content
+print(response)
