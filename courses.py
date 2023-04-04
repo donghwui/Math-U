9435bcfdbe9d97cb980735caf16559b8640601e9
adam <109483705+adam-han@users.noreply.github.com>
2023-04-03 23:38:34 -0400

    started approach with selenium

--- courses.py
+++ courses.py
@@ -1,28 +1,31 @@
 import csv
-from requests_html import HTMLSession
-from requests_html import AsyncHTMLSession
-from pyppeteer import launch
-import asyncio
+from selenium import webdriver
 
-PATH = '/opt/homebrew/bin/chromium'
+PATH = '/opt/homebrew/bin/chromedriver'
+driver = webdriver.Chrome(PATH)
 
-async def get_session():
-    browser = await launch(executable_path=PATH)
-    session = HTMLSession(browser_args=['--no-sandbox', '--disable-setuid-sandbox'])
-    session._browser = browser
-    return session
 
-async def course_scrapper(course_code):
-    session = await get_session()
-    page = session.get(f"https://uwflow.com/course/{course_code}")
+driver.get('https://uwflow.com/course/cs246')
 
-    page.html.render()
 
-    div_element = page.html.find('div.sc-ptDSg eYGQDi', first=True)
-    print(div_element)
-    await session.close()
 
-async def main():
-    await course_scrapper('cs246')
+# async def get_session():
+#     browser = await launch(executable_path=PATH)
+#     session = HTMLSession(browser_args=['--no-sandbox', '--disable-setuid-sandbox'])
+#     session._browser = browser
+#     return session
 
-asyncio.run(main())
+# async def course_scrapper(course_code):
+#     session = await get_session()
+#     page = session.get(f"https://uwflow.com/course/{course_code}")
+
+#     page.html.render()
+
+#     div_element = page.html.find('div.sc-ptDSg eYGQDi', first=True)
+#     print(div_element)
+#     await session.close()
+
+# async def main():
+#     await course_scrapper('cs246')
+
+# asyncio.run(main())
