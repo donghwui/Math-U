60021f80194a08fe22f0b19cff27b2346ef8c216
adam <109483705+adam-han@users.noreply.github.com>
2023-04-03 23:10:27 -0400

    updates to courses.py, not fully functioning yet

--- courses.py
+++ courses.py
@@ -1,17 +1,28 @@
 import csv
 from requests_html import HTMLSession
+from requests_html import AsyncHTMLSession
+from pyppeteer import launch
+import asyncio
 
+PATH = '/opt/homebrew/bin/chromium'
 
+async def get_session():
+    browser = await launch(executable_path=PATH)
+    session = HTMLSession(browser_args=['--no-sandbox', '--disable-setuid-sandbox'])
+    session._browser = browser
+    return session
 
-def course_scrapper(course_code):
-    session = HTMLSession()
-
+async def course_scrapper(course_code):
+    session = await get_session()
     page = session.get(f"https://uwflow.com/course/{course_code}")
 
     page.html.render()
 
     div_element = page.html.find('div.sc-ptDSg eYGQDi', first=True)
     print(div_element)
+    await session.close()
 
+async def main():
+    await course_scrapper('cs246')
 
-course_scrapper('cs246')
+asyncio.run(main())
